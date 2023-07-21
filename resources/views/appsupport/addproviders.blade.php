d3bf4423da5127f92daa05d45e5ba651e0a855c5
Ahlamelmoumen2.Ahlamelmoumen2 <Ahlamelmoumen2.ahlamelmoumen2@gmail.com>
2023-07-21 15:56:40 +0100

    first commit

--- resources/views/appsupport/addproviders.blade.php
+++ resources/views/appsupport/addproviders.blade.php
@@ -11,10 +11,52 @@
 <body>
     <div class="container">
         <div class="row">
-
-
             <div class="col s12">
-                <h1>ADD PROVIDERS</h1>
+               <h1>ADD PROVIDERS</h1>
+                    <div class="mb-3 row">
+                        <label for="name" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                          <input type="text"  class="form-control" id="name" >
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="link" class="col-sm-2 col-form-label">Link</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="link">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="user" class="col-sm-2 col-form-label">User</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="user">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="password" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="password">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="loctaion" class="col-sm-2 col-form-label">Loctaion</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="loctaion">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="comment" class="col-sm-2 col-form-label">Comment</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="comment">
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
 
 
 
@@ -29,13 +71,6 @@
 
              </div>
         </div>
-
-
-
-
-
-
-
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
