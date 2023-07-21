9e5b77acb7ff340a10ee0ac291adb86779d54dca
Ahlamelmoumen2.Ahlamelmoumen2 <Ahlamelmoumen2.ahlamelmoumen2@gmail.com>
2023-07-21 15:40:50 +0100

    first commit

--- resources/views/appsupport/layout.blade.php
+++ resources/views/appsupport/layout.blade.php
@@ -150,6 +150,7 @@
       </div>
     </div>
     <br><br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
 </body>
